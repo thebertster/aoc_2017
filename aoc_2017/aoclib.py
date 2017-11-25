28eab0033d9323ff0bee828b1a26dfb183deea12
Bert Casula <thebertster@gmail.com>
2017-11-25 18:56:37 +0000

    Getting ready for AOC 2017!

---
+++ aoc_2017/aoclib.py
@@ -0,0 +1,20 @@
+import os
+import requests
+
+class AOCLib:
+    _aoc_input_url = 'http://adventofcode.com/{year}/day/{day}/input'
+
+    def __init__(self, aoc_year):
+        self.aoc_year = aoc_year
+        user_profile = os.environ['USERPROFILE']
+
+        with open(user_profile + '\\aoc.cookie', 'r') as aoc_cookie_file:
+            aoc_cookie_value = aoc_cookie_file.read()
+
+        self._aoc_cookie = {'session':aoc_cookie_value}
+
+    def get_puzzle_input(self, day):
+        response = requests.get(
+            self._aoc_input_url.format(year = self.aoc_year, day = day),
+            cookies = self._aoc_cookie)
+        return response.text
--- aoc_2017/day_01.py
+++ aoc_2017/day_01.py
@@ -1,1 +1,6 @@
+import aoclib
 
+puzzle = (2016, 1)
+
+aoc = aoclib.AOCLib(puzzle[0])
+puzzle_input = aoc.get_puzzle_input(puzzle[1])
