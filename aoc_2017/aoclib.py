1b0780629ceaa167469b56d34e328118a88bf326
Bert Casula <thebertster@gmail.com>
2017-11-26 21:31:48 +0000

    The countdown has started!

--- aoc_2017/aoclib.py
+++ aoc_2017/aoclib.py
@@ -13,8 +13,19 @@ class AOCLib:
 
         self._aoc_cookie = dict(session=aoc_cookie_value)
 
-    def get_puzzle_input(self, day):
+    def get_puzzle_input(self, day, parse_function=lambda x: x):
         response = requests.get(
             self._aoc_input_url.format(year=self.aoc_year, day=day),
             cookies=self._aoc_cookie)
-        return response.text
+
+        puzzle_input = response.text.rstrip('\n')
+
+        return parse_function(puzzle_input)
+
+    @staticmethod
+    def to_list(puzzle_input):
+        return [element.strip() for element in puzzle_input.split(',')]
+
+    @staticmethod
+    def lines_to_list(puzzle_input):
+        return puzzle_input.split('\n')
--- aoc_2017/day_01.py
+++ aoc_2017/day_01.py
@@ -1,6 +1,15 @@
-import aoclib
+from aoclib import AOCLib
 
-puzzle = (2016, 1)
+puzzle = (2017, 1)
 
-aoc = aoclib.AOCLib(puzzle[0])
+# Initialise the helper library
+
+aoc = AOCLib(puzzle[0])
+
+# Get the puzzle input in appropriate format
+
 puzzle_input = aoc.get_puzzle_input(puzzle[1])
+
+# print(puzzle_input)
+
+# Puzzle solution:
