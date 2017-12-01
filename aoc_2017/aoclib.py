619626475abc2631c238d5a2c0471e5c3939eb49
Bert Casula <thebertster@gmail.com>
2017-12-01 18:30:34 +0000

    Solution to day 1

--- aoc_2017/aoclib.py
+++ aoc_2017/aoclib.py
@@ -4,6 +4,9 @@ import requests
 class AOCLib:
     _aoc_input_url = 'http://adventofcode.com/{year}/day/{day}/input'
 
+    # Initialise and fetch the session authentication cookie from
+    # the user profile directory to avoid statically coding it here
+
     def __init__(self, aoc_year):
         self.aoc_year = aoc_year
         user_profile = os.environ['USERPROFILE']
@@ -13,19 +16,34 @@ class AOCLib:
 
         self._aoc_cookie = dict(session=aoc_cookie_value)
 
-    def get_puzzle_input(self, day, parse_function=lambda x: x):
+    # Get the puzzle input from the AOC website and apply
+    # an optional transform function to it before returning
+
+    def get_puzzle_input(self, day, transform_function=lambda x: x):
         response = requests.get(
             self._aoc_input_url.format(year=self.aoc_year, day=day),
             cookies=self._aoc_cookie)
 
         puzzle_input = response.text.rstrip('\n')
 
-        return parse_function(puzzle_input)
+        return transform_function(puzzle_input)
+
+    # Various static methods for manipulating the puzzle input:
+
+    # Transform a comma-separated string to a list
 
     @staticmethod
     def to_list(puzzle_input):
         return [element.strip() for element in puzzle_input.split(',')]
 
+    # Transform multi-line input to a list
+
     @staticmethod
     def lines_to_list(puzzle_input):
         return puzzle_input.split('\n')
+
+    # Transform a sequence of digits to a list of integers
+
+    @staticmethod
+    def sequence_to_int(puzzle_input):
+        return [int(digit) for digit in puzzle_input]
--- aoc_2017/day_01.py
+++ aoc_2017/day_01.py
@@ -1,15 +1,35 @@
 from aoclib import AOCLib
 
+def solve_puzzle(circular_list, offset):
+    circle_size = len(circular_list)
+
+    running_total = 0
+
+    for index in range(circle_size):
+        index2 = (index + offset) % circle_size
+        if circular_list[index] == circular_list[index2]:
+            running_total += circular_list[index]
+
+    return running_total
+
 puzzle = (2017, 1)
 
 # Initialise the helper library
 
 aoc = AOCLib(puzzle[0])
 
-# Get the puzzle input in appropriate format
+# Get the puzzle input as a list of integers
 
-puzzle_input = aoc.get_puzzle_input(puzzle[1])
+puzzle_input = aoc.get_puzzle_input(puzzle[1], AOCLib.sequence_to_int)
 
 # print(puzzle_input)
 
-# Puzzle solution:
+# Puzzle solution part 1:
+
+print("Part 1 Solution: {}".
+      format(solve_puzzle(puzzle_input, 1)))
+
+# Puzzle solution part 2
+
+print("Part 2 Solution: {}".
+      format(solve_puzzle(puzzle_input, len(puzzle_input)//2)))
