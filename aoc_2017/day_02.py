22d1be3d393ab483e62ea59d71b062b2f640f5d8
Bert Casula <thebertster@gmail.com>
2017-12-02 15:49:31 +0000

    Additional solutions to day 2

--- aoc_2017/day_02.py
+++ aoc_2017/day_02.py
@@ -1,4 +1,6 @@
+from itertools import combinations
 from aoclib import AOCLib
+
 
 puzzle = (2017, 2)
 
@@ -22,18 +24,22 @@ checksum_1 = sum([max(row) - min(row) for row in spreadsheet])
 
 print('Puzzle Output 1: {}'.format(checksum_1))
 
-# Puzzle solution part 2 (without over-iteration!):
+# Puzzle solution part 2
+# Nested loops
+# Stop after match
+# No max() or min()
 
 checksum_2_1 = 0
 
 for row in spreadsheet:
     dividend = None
-    for col_1 in range(1, len(row)):
-        for col_2 in range(col_1):
-            numerator = row[col_1]
-            denominator = row[col_2]
-            if numerator < denominator:
-                numerator, denominator = denominator, numerator
+    for col_1 in range(1, len(row)):        
+        cell_1 = row[col_1-1]
+        for cell_2 in row[col_1:]:
+            if cell_1 > cell_2:
+                numerator, denominator = cell_1, cell_2
+            else:
+                numerator, denominator = cell_2, cell_1
             if numerator % denominator == 0:
                 dividend = (numerator // denominator)
                 break
@@ -45,11 +51,19 @@ print('Puzzle Output 2#1: {}'.format(checksum_2_1))
 
 # Alternative list comprehension "one-liner":
 
-checksum_2_2 = sum([numerator//denominator for (numerator, denominator) in
-                  [(row[i-1], cell) if row[i-1] > cell
-                   else (cell, row[i-1]) for row in
+checksum_2_2 = sum([max(pair)//min(pair) for pair in
+                  [(row[i-1], cell) for row in
                    spreadsheet for i in
                    range(1, len(row)) for cell in
-                   row[i:]] if numerator % denominator == 0])
+                   row[i:]] if max(pair) % min(pair) == 0])
 
 print('Puzzle Output 2#2: {}'.format(checksum_2_2))
+
+# Alternative itertools version:
+
+checksum_2_3 = sum([max(pair)//min(pair) for row in
+                    spreadsheet for pair in
+                    combinations(row, 2)
+                    if max(pair) % min(pair) == 0])
+
+print('Puzzle Output 2#3: {}'.format(checksum_2_3))
