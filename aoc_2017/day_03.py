ffeda6463ac6f7258a7c5c32526834dcf450d077
Bert Casula <thebertster@gmail.com>
2017-12-02 15:00:35 +0000

    Solution to day 2

---
+++ aoc_2017/day_03.py
@@ -0,0 +1,13 @@
+from aoclib import AOCLib
+
+puzzle = (2017, 3)
+
+# Initialise the helper library
+
+aoc = AOCLib(puzzle[0])
+
+# Get the puzzle input
+
+puzzle_input = aoc.get_puzzle_input(puzzle[1])
+
+# print(puzzle_input)
