1acc27cb0bdc50f465961d76d391374e1e453f17
Bert Casula <thebertster@gmail.com>
2017-12-04 20:18:32 +0000

    Solution to day 4

--- aoc_2017/day_04.py
+++ aoc_2017/day_04.py
@@ -8,6 +8,30 @@ aoc = AOCLib(puzzle[0])
 
 # Get the puzzle input
 
-puzzle_input = aoc.get_puzzle_input(puzzle[1], int)
+puzzle_input = aoc.get_puzzle_input(puzzle[1], AOCLib.lines_to_list)
 
 # print(puzzle_input)
+
+# Puzzle solution part 1
+
+validPassphrases = 0
+
+for passphrase in puzzle_input:
+    words = passphrase.split(' ')
+    if len(words) == len(set(words)):
+        validPassphrases += 1
+
+print('Puzzle Output 1: {}'.format(validPassphrases))
+
+# Puzzle solution part 2
+
+validPassphrases = 0
+
+for passphrase in puzzle_input:
+    words = [''.join(sorted(list(word)))
+             for word in passphrase.split(' ')]
+    print(words)
+    if len(words) == len(set(words)):
+        validPassphrases += 1
+
+print('Puzzle Output 2: {}'.format(validPassphrases))
