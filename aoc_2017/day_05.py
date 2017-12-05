8a3357b60c8f030418c8cb1154ab7cdcfbb3ade4
Bert Casula <thebertster+github@gmail.com>
2017-12-05 11:07:55 +0000

    Solution to day 5 ++

--- aoc_2017/day_05.py
+++ aoc_2017/day_05.py
@@ -1,5 +1,18 @@
 from aoclib import AOCLib
 
+def process_jump_list(jump_list, offset_function=lambda x: x + 1):
+    offset = 0
+    steps = 0
+    jump_list = jump_list[:]
+    jump_list_len = len(jump_list)
+    while offset < jump_list_len:
+        offset_inc = jump_list[offset]
+        jump_list[offset] = offset_function(jump_list[offset])
+        offset += offset_inc
+        steps += 1
+
+    return steps
+
 puzzle = (2017, 5)
 
 # Initialise the helper library
@@ -14,28 +27,12 @@ puzzle_input = aoc.get_puzzle_input(puzzle[1], AOCLib.lines_to_list_int)
 
 # Puzzle solution part 1
 
-offset = 0
-steps = 0
-jump_list = puzzle_input[:]
-jump_list_len = len(jump_list)
-while offset < jump_list_len:
-    old_offset = offset
-    offset += jump_list[old_offset]
-    jump_list[old_offset] += 1
-    steps += 1
+steps = process_jump_list(puzzle_input)
 
 print('Puzzle Output 1: {}'.format(steps))
 
 # Puzzle solution part 1
 
-offset = 0
-steps = 0
-jump_list = puzzle_input[:]
-jump_list_len = len(jump_list)
-while offset < jump_list_len:
-    offset_inc = jump_list[offset]
-    jump_list[offset] += 1 if jump_list[offset] < 3 else -1
-    offset += offset_inc
-    steps += 1
+steps = process_jump_list(puzzle_input, lambda x: x+1 if x<3 else x-1)
     
 print('Puzzle Output 2: {}'.format(steps))
