d318a7711954903874b3d028b3e28150cf4cfb36
Bert Casula <thebertster@gmail.com>
2017-12-19 08:50:41 +0000

    Solution to day 19

--- aoc_2017/day_19.py
+++ aoc_2017/day_19.py
@@ -8,14 +8,48 @@ aoc = AOCLib(puzzle[0])
 
 # Get the puzzle input
 
-puzzle_input = aoc.get_puzzle_input(puzzle[1])
+puzzle_input = aoc.get_puzzle_input(puzzle[1], AOCLib.lines_to_list)
 
 # print(puzzle_input)
 
+start_x = puzzle_input[0].index('|')
+
+directions = ((0, 1), (1, 0), (0, -1), (-1,0))
+
+direction = 0
+
+x = start_x
+y = 0
+
+path = []
+
+steps = 0
+
+while direction >= 0:
+    directions_to_check = (direction, (direction + 1) % 4, (direction -1) % 4)
+    for direction_to_check in directions_to_check:
+        sniff_x = x + directions[direction_to_check][0]
+        sniff_y = y + directions[direction_to_check][1]
+        sniffed_char = puzzle_input[sniff_y][sniff_x]
+        if sniffed_char != ' ':
+            break
+    else:
+        direction_to_check = -1
+
+    direction = direction_to_check
+
+    if puzzle_input[y][x].isalpha():
+        path.append(puzzle_input[y][x])
+
+    steps += 1
+
+    x = sniff_x
+    y = sniff_y
+
 # Puzzle solution part 1
 
-print('Puzzle Output 1: {}'.format('placeholder'))
+print('Puzzle Output 1: {}'.format(''.join(path)))
 
 # Puzzle solution part 2
 
-print('Puzzle Output 2: {}'.format('placeholder'))
+print('Puzzle Output 2: {}'.format(steps))
