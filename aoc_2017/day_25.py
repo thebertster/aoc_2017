9c737bc61b6e2fef18c5419408de2b1a496ac04b
Bert Casula <thebertster@gmail.com>
2017-12-25 09:54:16 +0000

    Solution to day 25!!! Merry Christmas!!!

--- aoc_2017/day_25.py
+++ aoc_2017/day_25.py
@@ -1,6 +1,6 @@
 from lib.aoclib import AOCLib
 
-puzzle = (2017, 24)
+puzzle = (2017, 25)
 
 # Initialise the helper library
 
@@ -8,14 +8,36 @@ aoc = AOCLib(puzzle[0])
 
 # Get the puzzle input
 
-puzzle_input = aoc.get_puzzle_input(puzzle[1])
+puzzle_input = aoc.get_puzzle_input(puzzle[1], AOCLib.lines_to_list)
 
 # print(puzzle_input)
 
-# Puzzle solution part 1
+begin_state = puzzle_input[0][-2]
+checksum_steps = int(puzzle_input[1].split()[-2])
 
-aoc.print_solution(1, 'placeholder')
+state_machine = {}
 
-# Puzzle solution part 2
+for state in range(3, len(puzzle_input), 10):
+    next_state_0 = (int(puzzle_input[state + 2][-2]),
+                    1 if puzzle_input[state + 3][-3] == 'h' else -1,
+                    puzzle_input[state + 4][-2])
+    next_state_1 = (int(puzzle_input[state + 6][-2]),
+                    1 if puzzle_input[state + 7][-3] == 'h' else -1,
+                    puzzle_input[state + 8][-2])
+    state_machine[puzzle_input[state][-2]] = (next_state_0,
+                                              next_state_1)
 
-aoc.print_solution(2, 'placeholder')
+# Puzzle solution
+
+cursor = 0
+state = begin_state
+tape = {}
+
+while checksum_steps:
+    current = tape.setdefault(cursor, 0)
+    tape[cursor] = state_machine[state][current][0]
+    cursor += state_machine[state][current][1]
+    state = state_machine[state][current][2]
+    checksum_steps -= 1
+
+aoc.print_solution(1, sum(tape.values()))
